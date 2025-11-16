cc14fa71f5973c6d425f1aeece2c420856a35353
Juan-J0rge <juancontatrabalho@gmail.com>
2025-11-16 16:32:35 -0300

    Removendo arquivos .d.ts e .map gerados automaticamente

--- src/casting.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * primeiramente irei falar do namespace, quando declaramos 2 variaveis iguais no TS ele não aceita
- * não importa se os arquivos forem diferentes. Então tenhos q criar pastas, não no VSCODE, mas
- * pastas virtuais, no proprio arquivo.
- *
- * dentro do namespace é necessario ';'
- */
-export {};
-//# sourceMappingURL=casting.d.ts.map
--- src/oo.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=oo.d.ts.map
